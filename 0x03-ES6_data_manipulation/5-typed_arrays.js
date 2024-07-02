f7cda8bb7ea9fdae57eb94a1e0c4dc7ecf79a8da
Jumoke1 <kazeemjumoke12@gmail.com>
2024-07-02 19:31:05 +0100

    set

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,8 @@
-export default createInt8TypedArray(length, position, value) {
-  if (position >= lenght){
+export default function createInt8TypedArray(length, position, value){
+  if(position >= length){
     throw new Error("Position outside range");
   }
-  const range = new DataView(new ArrayBuffer(length), 0,lenght);
-  range.setInt8(positon, value);
+  const range = new DataView(new ArrayBuffer(length), 0, length);
+  range.setInt8(position, value);
   return range;
 }
---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+export default function cleanSet(set, startString){
+  const str = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== "string"){
+    return "";
+  }
+  for (const value of set.values()){
+    if (typeof value === "string" && value.startsWith(startString)) {
+        const data = value.substring(startString.length);
+        if (data && data !== value) {
+            str.push(data);
+        }
+    }
+  } 
+  return str.join("-")
+}
