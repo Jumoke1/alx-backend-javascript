250061f3388aa8ee649a0c1e29a830bbe0f4a18c
Jumoke1 <kazeemjumoke12@gmail.com>
2024-07-03 05:47:31 +0100

    obj

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
 //function defifnition
-export default function getStudentsByLocation(students, city){
+export default function getStudentsByLocation(students, city) {
   return students.filer((student) => student.location === city);
 }
