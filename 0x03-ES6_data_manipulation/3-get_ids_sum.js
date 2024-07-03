ee47ee67791c81ce0f6fb70530f52ba04f25699f
Jumoke1 <kazeemjumoke12@gmail.com>
2024-07-03 05:42:26 +0100

    obj

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
-export default function getStudentIdsSum(students){
-  return students.reduce((sum, current) => sum + current.id, 0)      
+export default function getStudentIdsSum(students) {
+  return students.reduce((sum, current) => sum + current.id, 0);      
 }
