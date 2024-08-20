301ef5e03455e66fda0bad62b36dc6c0bfe66f6f
“Jumoke1” <kazeemjumoke12@gmail.com>
2024-08-20 22:35:48 +0000

    cosole

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+process.stdout.write('Welcome to Holberton School,what is your name?\n')
+
+process.stdin.on('readable', () =>{
+  const readInput = process.stdin.read();
+
+  if(readInput) {
+    process.stdout.write(`Your name is: ${readInput}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});	 
