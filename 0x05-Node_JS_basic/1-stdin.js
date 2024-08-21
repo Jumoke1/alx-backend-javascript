30b85e820d905d3e88fa17cd16b6c42771f58d20
“Jumoke1” <kazeemjumoke12@gmail.com>
2024-08-21 10:04:03 +0000

    node js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
-process.stdout.write('Welcome to Holberton School,what is your name?\n')
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', () =>{
+process.stdin.on('readable', () => {
   const readInput = process.stdin.read();
 
-  if(readInput) {
+  if (readInput) {
     process.stdout.write(`Your name is: ${readInput}`);
   }
 });
 
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
-});	 
+});
